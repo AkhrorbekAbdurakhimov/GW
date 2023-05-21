c01763ba872db26f13885539b1d8752803e56021
Akhrorbek Abdurakhimov <akhrorbekabdurakhimov@gmail.com>
2023-05-21 22:52:59 +0500

    minor changes

--- routes/themes/index.js
+++ routes/themes/index.js
@@ -2,17 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 const { Router } = require('express');
-const { v4: uuidv4 } = require('uuid');
-const multer = require('multer');
-const Promise = require('bluebird');
 
 const { db } = require('./../../database');
 const { replaceAll } = require('./../../utils');
 const { getThemesListSchema, createThemeSchema } = require('./schema');
 
 const ThemesDB = require('./db');
-
-const upload = multer();
 
 const getThemesList = async (req, res) => {
   const { error: err, value: v } = getThemesListSchema.validate(req.query);
@@ -31,6 +26,12 @@ const getThemesList = async (req, res) => {
 
 const createTheme = async (req, res) => {
 
+  if (!['teacher', 'student'].includes(req.user.role)) {
+    return res.status(403).send({
+      message: "Permission Denied"
+    })
+  }
+
   const { error: err, value: v } = createThemeSchema.validate(req.body);
   if (err) {
     return res.status(400).send({
@@ -42,62 +43,47 @@ const createTheme = async (req, res) => {
 
   const client = await db.getClient();
 
-  let files = [];
-  if (req.files && req.files.length) {
-    await Promise.each(req.files, async ({ originalname, buffer }) => {
-      try {
-        let filename = `${uuidv4()}${path.extname(originalname)}`
-        fs.writeFileSync(path.join(process.cwd(), 'uploads', filename), buffer)
-        files.push(`('${filename}')`);
-      } catch (err) {
-        console.log(err);
-      }
-    })
-  }
-
   let processId = null;
 
   try {
     await client.query('BEGIN TRANSACTION');
 
-    let fileIds = [];
-    if (files.length) {
-      fileIds = (await client.query(`
-        INSERT INTO diploma.files (
-          filename
-        ) VALUES ${files.join(',')}
-        RETURNING id
-      `)).rows || [];
-    }
-
-    console.log(fileIds.map(file => file.id).join(','));
-
     const themeR = (await client.query(`
       INSERT INTO diploma.themes (
         title,
         description,
-        created_user_id,
-        file_ids
+        created_user_id
       ) VALUES (
-        $1, $2, $3, ${files.length ? `'{${fileIds.map(file => file.id).join(',')}}'` : 'NULL'}
+        $1, $2, $3
       ) RETURNING id;
     `, [v.title, v.description, req.user.id])).rows || [];
 
-    if (v.advisorId && themeR.length) {
-      const processR = (await client.query(`
-        INSERT INTO diploma.process (
-          theme_id,
-          attached_user_id,
-          advisor_id,
-          status
-        ) VALUES (
-          $1, $2, $3, 'initialized'
-        ) RETURNING id
-      `, [themeR[0].id, req.user.id, v.advisorId])).rows || [];
+    let attachedUserId = null;
+    let status = 'not-selected';
+    let advisorId = v.advisorId ? v.advisorId : null;
 
-      if (processR.length) {
-        processId = processR[0].id;
-      }
+    if (req.user.role === 'student') {
+      attachedUserId = req.user.id;
+      status = 'initialized'
+    }
+
+    if (req.user.role === 'teacher') {
+      advisorId = req.user.id;
+    }
+
+    const processR = (await client.query(`
+      INSERT INTO diploma.process (
+        theme_id,
+        attached_user_id,
+        advisor_id,
+        status
+      ) VALUES (
+        $1, $2, $3, $4
+      ) RETURNING id
+    `, [themeR[0].id, attachedUserId, advisorId, status])).rows || [];
+
+    if (processR.length) {
+      processId = processR[0].id;
     }
 
     await client.query('COMMIT TRANSACTION')
@@ -116,6 +102,6 @@ const createTheme = async (req, res) => {
 const router = Router();
 
 router.get('/list', getThemesList);
-router.post('/create', upload.any(), createTheme);
+router.post('/create', createTheme);
 
 module.exports = router;
